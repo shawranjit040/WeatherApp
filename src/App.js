e739012f6b93a586ead6138c1e3ba820b8fb1af3
shawranjit040 <126514491+shawranjit040@users.noreply.github.com>
2023-09-19 22:57:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,20 +5,25 @@ import axios from "axios";
 
 function App() {
   const apiKey = 'b7c0431351mshf1efdaeeee8ffc5p175f36jsn2eb1f570f414';
-  
+  const apiUrl = "https://open-weather13.p.rapidapi.com/city/landon";
+
   const [inputCity, setInputCity] = useState("");
   const [data, setData] = useState({});
 
   const getWeatherDetails = (cityName) => {
     if (!cityName) return;
-    const apiUrl =
-       "https://api.openweathermap.org/data/2.5/weather?q=" 
-       +
-      cityName +
-      "&appid=" +
-      apiKey;
+
+    const options = {
+      method: 'GET',
+      url: apiUrl,
+      headers: {
+        'X-RapidAPI-Key': apiKey,
+        'X-RapidAPI-Host': 'open-weather13.p.rapidapi.com'
+      },
+    };
+
     axios
-      .get(apiUrl)
+      .request(options)
       .then((res) => {
         console.log("response", res.data);
         setData(res.data);
@@ -27,6 +32,7 @@ function App() {
         console.log("err", err);
       });
   };
+
   const handleChangeInput = (e) => {
     console.log("value", e.target.value);
     setInputCity(e.target.value);
@@ -35,10 +41,6 @@ function App() {
   const handleSearch = () => {
     getWeatherDetails(inputCity);
   };
-
-  //  useEffect(() => {
-  //   getWeatherDetails("damoh")
-  //  })
 
   return (
     <div className="col-md-12">
@@ -60,21 +62,18 @@ function App() {
           </button>
         </div>
       </div>
-       {/* {Object.keys(data).length > 0 &&  */}
-        <div className="col-md-12 text-center mt-5">
-          <div className="shadow rounded wetherResultBox">
-            <img
-              className="wetherIcon"
-              src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-512.png "
-            />
-
-            <h5 className="wetherCity">{data?.name}</h5>
-            <h6 className="wetherTemperature">
-              {(data?.main?.temp - 276.15).toFixed()}°C
-            </h6>
-          </div>
+      <div className="col-md-12 text-center mt-5">
+        <div className="shadow rounded wetherResultBox">
+          <img
+            className="wetherIcon"
+            src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-512.png "
+          />
+          <h5 className="wetherCity">{data?.name}</h5>
+          <h6 className="wetherTemperature">
+            {(data?.main?.temp - 276.15).toFixed()}°C
+          </h6>
         </div>
-
+      </div>
     </div>
   );
 }
